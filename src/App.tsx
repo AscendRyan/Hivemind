c19561d82be1f64d404e58b12d1a4202e5bb0ff6
AscendRyan <ryankachf@gmail.com>
2025-09-22 21:18:17 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Careers from "./pages/Careers";
 import Blog from "./pages/Blog";
+import BlogPost from "./pages/BlogPost"; // <-- NEW: single post page
 import NotFound from "./pages/NotFound";
-import BlogPost from "./pages/BlogPost";
 
 const queryClient = new QueryClient();
 
@@ -21,7 +21,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/careers" element={<Careers />} />
           <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:slug" element={<BlogPost />} />
+          <Route path="/blog/:slug" element={<BlogPost />} /> {/* <-- NEW route */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
