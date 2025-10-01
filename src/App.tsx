ecb6ddff12c5d8908a5d012ed96c2dd1bfa8c657
AscendRyan <ryankachf@gmail.com>
2025-10-01 20:43:14 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import Blog from "./pages/Blog";
 import BlogPost from "./pages/BlogPost";
 import NotFound from "./pages/NotFound";
 import Error404 from "./pages/Error404";
-import WPShell from "./pages/WPShell";
+import Skin from "./pages/Skin";
 
 const queryClient = new QueryClient();
 
@@ -27,7 +27,7 @@ const App = () => (
             <Route path="/blog" element={<Blog />} />
             <Route path="/blog/:slug" element={<BlogPost />} />
             <Route path="*" element={<Error404 />} />
-            <Route path="/wp-shell" element={<WPShell />} />
+            <Route path="/skin" element={<Skin />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
